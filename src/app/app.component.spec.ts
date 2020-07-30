e9f6c6dd3aacfcb3761d7f9aa95abb57f3184876
Andi Meier <andi@daemeier-clan.ch>
2020-07-30 11:57:33 +0200

    Make app.component minimalistic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-rxjs-trial app is running!');
+    expect(compiled.querySelector('p').textContent).toContain('angular-rxjs-trial app is running!');
   });
 });
